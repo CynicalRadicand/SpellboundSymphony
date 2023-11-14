7380fbc20dc36a6c276e2b3eae75bcea61ca9531
AoMedium <anoddmedium@gmail.com>
2023-11-14 13:08:58 +1300

    remove redundant null

--- Godot/Scripts/RuneInput/RuneInputManager.cs
+++ Godot/Scripts/RuneInput/RuneInputManager.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class RuneInputManager : Node
 {
-	[Export] private NodePath storePresenterPath = null;
+	[Export] private NodePath storePresenterPath;
 	RuneStorePresenter storePresenter;
 
 	// Called when the node enters the scene tree for the first time.
