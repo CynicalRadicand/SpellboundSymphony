f1f9166e389a7a163b38ead664a775c5a63fbf4f
Samuel liu <86864666+CynicalRadicand@users.noreply.github.com>
2023-11-27 21:14:12 +1300

    Implement blank interactable

--- Godot/Scripts/Player/player.cs
+++ Godot/Scripts/Player/player.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Microsoft.VisualBasic;
 using System;
 
 public partial class player : RigidBody3D
@@ -6,10 +7,19 @@ public partial class player : RigidBody3D
 	[Export] private float playerSpeed = 1000;
     [Export] private float jumpForce = 500;
 
+    private Area3D interactArea;
+
+    private bool interacting;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
-	}
+
+        interactArea = GetNode<Area3D>("InteractArea");
+
+        interactArea.BodyEntered += InteractEnter;
+        interactArea.BodyExited += InteractExit;
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
@@ -22,6 +32,11 @@ public partial class player : RigidBody3D
 		if (Input.IsActionJustPressed("Jump") && isOnGround())
 		{
 			ApplyCentralForce(Vector3.Up * jumpForce);
+		}
+
+		if (Input.IsActionJustPressed("Interact") && interacting)
+		{
+			GD.Print("Interacting");
 		}
 	}
 
@@ -38,4 +53,20 @@ public partial class player : RigidBody3D
 		}
 		return false;
 	}
+    private void InteractEnter(Node3D body)
+    {
+        if (body.IsInGroup("Interactable"))
+        {
+            interacting = true;
+        }
+    }
+
+    private void InteractExit(Node3D body)
+    {
+        if (body.IsInGroup("Interactable"))
+        {
+            interacting = false;
+        }
+    }
+
 }
