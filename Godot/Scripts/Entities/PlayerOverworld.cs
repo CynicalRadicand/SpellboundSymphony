073bb037af81ec804d2045d50bb2b83effed117d
Samuel liu <86864666+CynicalRadicand@users.noreply.github.com>
2024-02-17 11:55:02 +1300

    Refactor Jump Code

--- Godot/Scripts/Entities/PlayerOverworld.cs
+++ Godot/Scripts/Entities/PlayerOverworld.cs
@@ -31,33 +31,18 @@ public partial class PlayerOverworld : Node2D
 	public override void _PhysicsProcess(double delta)
 	{
 
-
 		direction = Input.GetActionStrength("MoveRight") - Input.GetActionStrength("MoveLeft");
 
-		bool jump = Input.IsActionPressed("MoveUp");
-
-		if (jump && grounded) {
-			velocity.Y = jumpForce;
-			localHoldTime = jumpHoldTime;
-            animationPlayback.Travel("Jump");
-        } else if (localHoldTime > 0) { 
-			if (jump)
-			{
-				velocity.Y = jumpForce;
-			} else
-			{
-				localHoldTime = 0;
-			}
-		}
-
-		localHoldTime -= delta;
+        Jump();
+        localHoldTime -= delta;
 
         velocity.X = Mathf.MoveToward(velocity.X, maxRun * direction, runAccel * (float)delta);
-
         velocity.Y = Mathf.MoveToward(velocity.Y, maxFall, gravity * (float)delta);
+
 
         GlobalPosition += new Vector2(velocity.X * (float)delta, velocity.Y * (float)delta);
 
+        // Fake Floor, remove later
 		if (GlobalPosition.Y >= 200)
 		{
 			GlobalPosition = new Vector2(GlobalPosition.X, 200);
@@ -72,6 +57,30 @@ public partial class PlayerOverworld : Node2D
 
 	}
 
+	private void Jump()
+	{
+        bool jump = Input.IsActionPressed("MoveUp");
+
+        if (jump && grounded)
+        {
+            velocity.Y = jumpForce;
+            localHoldTime = jumpHoldTime;
+            animationPlayback.Travel("Jump");
+        }
+        else if (localHoldTime > 0)
+        {
+            if (jump)
+            {
+                velocity.Y = jumpForce;
+            }
+            else
+            {
+                localHoldTime = 0;
+            }
+        }
+
+    }
+
 	private void UpdateAnimation()
 	{
 		if(velocity.X == 0)
@@ -85,7 +94,18 @@ public partial class PlayerOverworld : Node2D
             animation.Set("parameters/conditions/moving", true);
         }
 
-		if (direction != 0)
+        if (grounded)
+        {
+            animation.Set("parameters/conditions/grounded", true);
+            animation.Set("parameters/conditions/airborne", false);
+        }
+        else
+        {
+            animation.Set("parameters/conditions/grounded", false);
+            animation.Set("parameters/conditions/airborne", true);
+        }
+
+        if (direction != 0)
 		{
             animation.Set("parameters/Idle/blend_position", direction);
             animation.Set("parameters/Run/blend_position", direction);
@@ -93,16 +113,5 @@ public partial class PlayerOverworld : Node2D
             animation.Set("parameters/Fall/blend_position", direction);
         }
 
-		if (grounded)
-		{
-            animation.Set("parameters/conditions/grounded", true);
-            animation.Set("parameters/conditions/airborne", false);
-        }
-		else
-		{
-            animation.Set("parameters/conditions/grounded", false);
-            animation.Set("parameters/conditions/airborne", true);
-        }
-		
     }
 }
